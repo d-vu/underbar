8684170a874d70f527e3956e76d3c16a58c356b6
Daniel Vu <daniel.h.vu@gmail.com>
2016-02-28 17:03:52 -0600

    Implemented ._filter and ._reject.

--- src/underbar.js
+++ src/underbar.js
@@ -61,7 +61,6 @@
         iterator(collection[key], key, collection);
       }
     }
-
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -83,12 +82,22 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    _.each(collection, function(item){
+      if (test(item)){
+        passed.push(item);
+      }
+    });
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection,function(item){
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
