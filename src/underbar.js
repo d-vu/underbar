d74ff50215d7fb1e0ef1409e847ddc268f786874
Daniel Vu <daniel.h.vu@gmail.com>
2016-02-29 23:34:39 -0600

    Debugging _.every. I think I almost got it!

--- src/underbar.js
+++ src/underbar.js
@@ -204,6 +204,16 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+
+    return _.reduce(collection, function(matchTest, item){
+
+      if(matchTest){
+        matchTest = iterator(item);
+      }
+      return matchTest;
+
+    }, true);
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
